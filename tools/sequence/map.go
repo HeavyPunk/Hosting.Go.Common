df82d2997b1fad049b0a299e46373cd4075c4eb4
HeavyPunk <kirillkuchma8@gmail.com>
2024-01-25 15:31:14 +0500

    Added mapper with index of element

--- tools/sequence/map.go
+++ tools/sequence/map.go
@@ -7,3 +7,11 @@ func Mapper[TIn any, TOut any](input []TIn, mapper func(TIn) TOut) []TOut {
 	}
 	return res
 }
+
+func MapperIt[TIn any, TOut any](input []TIn, mapper func(int, TIn) TOut) []TOut {
+	res := make([]TOut, len(input))
+	for i := 0; i < len(input); i++ {
+		res[i] = mapper(i, input[i])
+	}
+	return res
+}
